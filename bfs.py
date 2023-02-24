76ce6931f228c71aa872a31423ef80f5d9b97f42
Marjan Fariborz <mfariborz@ucdavis.edu>
2023-02-24 15:30:34 -0800

    Adding the first version of bfs

---
+++ bfs.py
@@ -0,0 +1,134 @@
+import json
+import math
+import argparse
+
+def read_inputs():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("graph_file", type=str)
+    argparser.add_argument("truth_path", type=str)
+    argparser.add_argument("num_slices", type=int)
+    args = argparser.parse_args()
+    return args.graph_file, args.truth_path, args.num_slices
+
+def process_graph(graph_file):
+    curr_src_id = -1
+    curr_edge_index = 0
+    curr_num_edges = 0
+    max_dst_id = -1
+
+    edge_indices = []
+    edges = []
+
+    src_id = -1
+    dst_id = -1
+
+    for line in graph_file.readlines():
+        src_id, dst_id = line.split()
+        src_id = int(src_id)
+        dst_id = int(dst_id)
+
+        if dst_id > max_dst_id:
+            max_dst_id = dst_id
+
+        if src_id != curr_src_id:
+            if curr_src_id != -1:
+                edge_indices.append(curr_edge_index)
+                curr_edge_index += curr_num_edges
+                curr_num_edges = 0
+            for _ in range(curr_src_id + 1, src_id):
+                edge_indices.append(curr_edge_index)
+            curr_src_id = src_id
+            edges.append(dst_id)
+            curr_num_edges += 1
+        else:
+            edges.append(dst_id)
+            curr_num_edges += 1
+    edge_indices.append(curr_edge_index)
+    curr_edge_index += curr_num_edges
+
+    for _ in range(src_id + 1, max_dst_id + 1):
+        edge_indices.append(curr_edge_index)
+    return edge_indices, edges
+
+def bfs(edge_indices, edges):
+    heights = [1000 for _ in range(len(edge_indices))]
+    frontier = [0]
+    heights[0] = 0
+
+    while True:
+        next_frontier = []
+        for curr_id in frontier:
+            start = edge_indices[curr_id]
+            if (curr_id != len(edge_indices) - 1):
+                end = edge_indices[curr_id + 1]
+            else:
+                end = len(edge_indices)
+            curr_edges = edges[start : end]
+
+            for edge in curr_edges:
+                if heights[edge] > (heights[curr_id] + 1):
+                    heights[edge] = heights[curr_id] + 1
+                    next_frontier.append(edge)
+        frontier = next_frontier
+        if len(frontier) == 0:
+            break
+    return heights
+
+def bfs_slice(edge_indices, edges, num_slices, slice_size):
+    print("Total number of slices: ",num_slices, " Slice size: ", slice_size)
+    heights = [1000 for _ in range(len(edge_indices))]
+    frontier = [[]  for _ in range(num_slices)]
+    frontier[0] = [0]
+    heights[0] = 0
+    count = 0
+    slices = 1
+    while True:
+        for slice in range(num_slices):
+            next_frontier = []
+            if (len(frontier[slice]) == 0):
+                continue
+            slices += 1
+            for curr_id in frontier[slice]:
+                start = edge_indices[curr_id]
+                if (curr_id != len(edge_indices) - 1):
+                    end = edge_indices[curr_id + 1]
+                else:
+                    end = len(edge_indices)
+                curr_edges = edges[start : end]
+                for edge in curr_edges:
+                    slice_index = math.floor(edge/slice_size)
+                    if heights[edge] > (heights[curr_id] + 1):
+                        heights[edge] = heights[curr_id] + 1
+                        if slice_index == slice:
+                            next_frontier.append(edge)
+                        else:
+                            frontier[slice_index].append(edge)
+            frontier[slice] = next_frontier
+            
+            count = 0
+            for f in frontier:
+                if len(f) == 0:
+                    count += 1
+            if count == len(frontier):
+                break
+        if count == len(frontier):
+            break
+    print(slices)
+    return heights
+
+if __name__ == "__main__":
+
+    graph_file_name, truth_file_path, num_slices = read_inputs()
+
+    edge_indices = []
+    edges = []
+    with open(graph_file_name, "r") as graph_file:
+        edge_indices, edges = process_graph(graph_file)
+
+    num_vertex_slice = math.ceil(float(len(edge_indices)/num_slices))
+    # print(len(edge_indices), num_slice, num_vertex_slice)
+    heights = bfs_slice(edge_indices, edges, num_slices, num_vertex_slice) 
+   
+
+    with open(f"{truth_file_path}/truth2.json", "w") as truth_file:
+        json.dump(heights, truth_file)
